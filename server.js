4615ab606d1e7b078a7f27674435525fea760a90
iwona <iwo.jarek@gmail.com>
2022-10-04 12:52:03 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use("/user", (req, res, next) => {
+  res.show("forbidden.html");
+  next();
+});
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+  res.show("home.html");
+});
+
+app.get("/home", (req, res) => {
+  res.show("home.html");
+});
+
+app.get("/about", (req, res) => {
+  res.show("about.html");
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, `/public/404.png`));
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port: 8000");
+});
